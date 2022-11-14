f46857bc545999d8b83d8473dc166584c4e46902
Mauran Pavan <mauranpavan@gmail.com>
2022-11-13 19:24:54 -0500

    separated db connection info as it is used multiple times

---
+++ src/main/java/soen387/DatabaseConnConstants.java
@@ -0,0 +1,10 @@
+package soen387;
+
+public final class DatabaseConnConstants {
+
+    private DatabaseConnConstants() {
+    }
+
+    public static final String DB_USER = "admin";
+    public static final String DB_PASSWORD = "admin";
+}
