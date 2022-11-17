d34a749c1546caf2c5499d504a2ee49035e01a80
Semi-Demi <91032290+Semi-Demi@users.noreply.github.com>
2022-11-17 16:06:10 -0500

    Add files via upload

--- src/main/java/dao/LoginDao.java
+++ src/main/java/dao/LoginDao.java
@@ -1,37 +1,44 @@
-package dao;
-
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.concurrent.*;
 
 import bean.LoginBean;
 
-public class LoginDao {
+public class LoginDao extends Thread{
+
+    Semaphore sem;
 
     public int getUserStatus(LoginBean loginBean) throws ClassNotFoundException {
         int status = 2;
 
         Class.forName("com.mysql.cj.jdbc.Driver");
+        
+        
 
         try (Connection connection = DriverManager
                 .getConnection("jdbc:mysql://localhost:3306/soen387_school", "root", "synystergates");
 
              // Step 2:Create a statement using connection object
-             PreparedStatement preparedStatement = connection
+        	sem.acquire(); 
+        	PreparedStatement preparedStatement = connection
                      .prepareStatement("select * from user where id = ? and password = ? ")) {
             preparedStatement.setString(1, loginBean.getUsername());
             preparedStatement.setString(2, loginBean.getPassword());
 
             ResultSet rs = preparedStatement.executeQuery();
+            sem.release();
             if (rs.next()) {
                 status = rs.getInt("isAdmin");
             }
+            
+            
 
         } catch (SQLException e) {
             // process sql exception
             printSQLException(e);
+            sem.release();
         }
         return status;
     }
